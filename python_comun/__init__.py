6e333ab3078ce9b994d476b9e0cfe6eb2c951c90
pedro <pedro@pedro-Despacho>
2026-09-23 14:28:54 +0200

    Ajustar estructura de la librería

--- python_comun/__init__.py
+++ python_comun/__init__.py
@@ -1,5 +1,16 @@
 """
 Python_Comun - Librería de funciones comunes
+
+NOTA: Para rehacer toda la estructura de librerías en la aplicación,
+se puede eliminar todo y volver a crearlo:
+
+Desde un terminal, estando con el entorno activo:
+$ source .venv/bin/activate
+Hacemos:
+$ rm uv.lock
+$ uv lock
+$ uv sync
+
 """
 from python_comun.funciones_comunes import *
 from python_comun.ventana_inicio import *
