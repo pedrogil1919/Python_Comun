ab22c8529b19a9f4e6828ff65c394077661c24d6
pedro <pedro@pedro-Despacho>
2026-09-24 15:07:25 +0200

    Añadir instrucciones generales para configuración de librerías.

--- python_comun/__init__.py
+++ python_comun/__init__.py
@@ -7,9 +7,30 @@ se puede eliminar todo y volver a crearlo:
 Desde un terminal, estando con el entorno activo:
 $ source .venv/bin/activate
 Hacemos:
+
+Si queremos utilizar la librería en modo edición, vamos al archivo project.toml, y en la
+entrada de la librería, hay que poner editable=true. Por ejemplo:
+
+python-xml = { path = "../../Python_XML", editable=true }
+
+Si queremos que la librería se la descargue de github (proyecto público), en el toml hay que
+poner:
+
+python-xml = { git = "https://github.com/pedrogil1919/Python_XML.git" }
+
+Sin embargo, en general para que los cambios tengan efecto, suele ser necesearioo borrar todo
+y volver a crearlo. Desde un terminal en el directorio donde está el toml:
+
+$ deactivate
+$ rm -rf .venv
+$ python3 -m venv .venv
+Y volvemos a activar el entorno virtual:
+$ source .venv/bin/activate
+# Borrar todo y volver a crearlo.
 $ rm uv.lock
 $ uv lock
 $ uv sync
+
 
 """
 from python_comun.funciones_comunes import *
